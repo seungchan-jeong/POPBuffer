0a6bd6767920ec79fd7e802428b0b6194eed1134
WIN-SEUNGCHAN-J\seungchan.jeong <seungchan.jeong@unity3d.com>
2023-10-13 20:42:22 +0900

    submesh가 있을 때 각 submesh에 pop 적용하는 것 구현

--- Assets/Scripts/POPGenerator.cs
+++ Assets/Scripts/POPGenerator.cs
@@ -41,18 +41,19 @@ public class POPGenerator : MonoBehaviour
 
     private void ApplyPOPBuffer(Mesh mesh, int quantizationLevel)
     {
-        (NativeArray<float3> verticesNative, NativeArray<uint> indicesNative, NativeArray<float2> uvNative) = POPBuffer.Decode(_popBuffer, quantizationLevel);
+        (NativeArray<float3> verticesNative, NativeArray<uint> indicesNative, NativeArray<float2> uvNative, List<int> subMeshIndexCount) = POPBuffer.Decode(_popBuffer, quantizationLevel);
         
         int vertexAttributeCount = 2; //Position UV (Normal, Tangent, Vertex Color etc...)
         int vertexCount = verticesNative.Length;
         int triangleIndexCount = indicesNative.Length;
 
         Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1); //TODO : submesh Count로 대체
+        //?? 이걸 submesh Count로 바꾸면, meshData마다 똑같은 vertex를 설정해줘야하는건가? 아니면 0번째만 vertex설정해주면 되는건가? 알아보기 
         Mesh.MeshData meshData = meshDataArray[0];
 
         var vertexAttributes = new NativeArray<VertexAttributeDescriptor>(vertexAttributeCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
         vertexAttributes[0] = new VertexAttributeDescriptor(dimension: 3);
-        vertexAttributes[1] = new VertexAttributeDescriptor(VertexAttribute.TexCoord0, dimension: 2, stream: 1); //position만 stream 0 나머진 다 stream 1
+        vertexAttributes[1] = new VertexAttributeDescriptor(VertexAttribute.TexCoord0, dimension: 2, stream: 1); //TODO : position만 stream 0 나머진 다 stream 1
         meshData.SetVertexBufferParams(vertexCount, vertexAttributes);
         vertexAttributes.Dispose();
 
@@ -66,14 +67,17 @@ public class POPGenerator : MonoBehaviour
         NativeArray<uint> indices = meshData.GetIndexData<uint>();
         indices.CopyFrom(indicesNative);  //TODO 아에 여기다가 맨 처음부터 카피하도록??
 
-        meshData.subMeshCount = 1;
-        meshData.SetSubMesh(0, new SubMeshDescriptor(0, triangleIndexCount)
-        // {
-        //     bounds = mesh.bounds, //TODO POP bound로 대체
-        //     vertexCount = vertexCount
-        // }, MeshUpdateFlags.DontRecalculateBounds
-        );
-
+        meshData.subMeshCount = mesh.subMeshCount;
+        for (int i = 0; i < subMeshIndexCount.Count; i++)
+        {
+            meshData.SetSubMesh(i, new SubMeshDescriptor(i == 0 ? 0 : subMeshIndexCount[i-1], subMeshIndexCount[i])
+                // {
+                //     bounds = mesh.bounds, //TODO POP bound로 대체
+                //     vertexCount = vertexCount
+                // }, MeshUpdateFlags.DontRecalculateBounds
+            );
+        }
+        
         // mesh.bounds = mesh.bounds;  //TODO POP bound로 대체
         Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
         mesh.RecalculateNormals();
@@ -86,17 +90,41 @@ public class POPGenerator : MonoBehaviour
 
 struct Level
 {
-    public Level(List<Vector3Int> cells, List<Vector3> positions, List<Vector2> uvs)
+    public Level(CellPerLevel cells, List<Vector3> positions, List<Vector2> uvs)
     {
         this.cells = cells;
         this.positions = positions;
         this.uvs = uvs;
     }
-
-    public List<Vector3Int> cells { get; } //삼각형 index
+    
+    public CellPerLevel cells { get; } //삼각형 index
     public List<Vector3> positions { get; } //삼각형 vertex
+    public List<Vector2> uvs { get; }
+}
 
-    public List<Vector2> uvs;
+struct CellPerSubmesh
+{
+    public List<Vector3Int> cells;
+
+    public CellPerSubmesh(List<Vector3Int> cells)
+    {
+        this.cells = cells;
+    }
+
+    public CellPerSubmesh(int count)
+    {
+        cells = new List<Vector3Int>(count);
+    }
+}
+struct CellPerLevel
+{
+    //TODO 아래 collection을 field로 접근하게 하지말고, 이 struct/class자체가 [,]를 override해서 접근할 수 있게 바꿔야 코드가 깔끔해짐.
+    public List<CellPerSubmesh> cellPerSubmeshes; //List<List<Vector3Int>> 
+
+    public CellPerLevel(int count)
+    {
+        cellPerSubmeshes = Enumerable.Range(0, count).Select(_ => new CellPerSubmesh(0)).ToList(); 
+    }
 }
 
 class POPBuffer
@@ -104,11 +132,25 @@ class POPBuffer
     private Bounds boundingBox;
     private List<Level> levels; //TODO streaming을 하지 않을 거라면 level별로 position을 나눌 필요가 없다. index만 나눠지면 된다. 
 
-    public static (NativeArray<float3> vertices, NativeArray<uint> indices, NativeArray<float2> uvs) Decode(POPBuffer popBuffer, int quantizationLevel)
+    public static (NativeArray<float3> vertices, NativeArray<uint> indices, NativeArray<float2> uvs, List<int> subMeshIndexCount) Decode(POPBuffer popBuffer, int quantizationLevel)
     {
-        //현재 quantizationLevel이 32이상일 경우 indices에서 문제가 있는 듯함. (31일 때랑 index, vertex 갯수가 같은데 이상하게 에러가 생김)
-        List<uint> indices = popBuffer.levels.Take(quantizationLevel).Select(level => level.cells)
-            .SelectMany(cells => cells).SelectMany(tri => new uint[]{ (uint)tri.x, (uint)tri.y, (uint)tri.z}).ToList();
+        List<uint> indices = new List<uint>();
+        List<int> subMeshStartAndCount = new List<int>();
+        int submeshCount = popBuffer.levels[0].cells.cellPerSubmeshes.Count;
+        for (int i = 0; i < submeshCount; i++)
+        {
+            for (int j = 0; j < quantizationLevel; j++)
+            {
+                indices.AddRange(popBuffer.levels[j].cells.cellPerSubmeshes[i].cells.SelectMany(tri => new uint[]{ (uint)tri.x, (uint)tri.y, (uint)tri.z}));
+            }
+            subMeshStartAndCount.Add(indices.Count - subMeshStartAndCount.Sum());
+        }
+        
+        // List<uint> indices = popBuffer.levels.Take(quantizationLevel)
+        //     .Select(level => level.cells.cellPerSubmeshes).SelectMany(cellPerSubmeshes => cellPerSubmeshes)
+        //     .Select(cellPerSubmesh => cellPerSubmesh.cells).SelectMany(cells => cells)
+        //     .SelectMany(tri => new uint[]{ (uint)tri.x, (uint)tri.y, (uint)tri.z}).ToList();
+        
         List<Vector3> positions = popBuffer.levels.Take(quantizationLevel).Select(level => level.positions)
             .SelectMany(positions => positions).ToList();
         List<Vector2> uvs = popBuffer.levels.Take(quantizationLevel).Select(level => level.uvs)
@@ -120,8 +162,7 @@ class POPBuffer
             positions = RescaleVertices(positions, popBuffer.boundingBox);
         }
         
-        
-        return (GetNativeVertexArrays(positions.ToArray()), new NativeArray<uint>(indices.ToArray(), Allocator.Temp), GetNativeVertexArrays(uvs.ToArray()));
+        return (GetNativeVertexArrays(positions.ToArray()), new NativeArray<uint>(indices.ToArray(), Allocator.Temp), GetNativeVertexArrays(uvs.ToArray()), subMeshStartAndCount);
     }
     
     public static unsafe NativeArray<float3> GetNativeVertexArrays(Vector3[] vertexArray) //https://gist.github.com/LotteMakesStuff/c2f9b764b15f74d14c00ceb4214356b4
@@ -166,28 +207,32 @@ class POPBuffer
 
     public static POPBuffer GeneratePopBuffer(Mesh mesh)
     {
-        int[] indices = mesh.GetIndices(0); //TODO 이거 nativeArray PTR로 복사비용 없앨 수 없나?
-        List<Vector3Int> cells = indices.Select((elem, index) => new { Elem = elem, Index = index })
-            .GroupBy(x => x.Index / 3)
-            .Select(group => new Vector3Int(
-                group.ElementAt(0).Elem,
-                group.ElementAt(1).Elem,
-                group.ElementAt(2).Elem)).ToList(); //TODO 이렇게 긴 LINQ, 최선인가..?
-
         List<Vector3> positions = new List<Vector3>();
         mesh.GetVertices(positions);
 
         List<Vector2> uvs = new List<Vector2>();
         mesh.GetUVs(0, uvs);
 
-        return Encode(mesh.bounds, cells, positions, uvs, 32);
+        List<CellPerSubmesh> cellPerSubMeshes = new List<CellPerSubmesh>();
+        for (int i = 0; i < mesh.subMeshCount; i++)
+        {
+            int[] indices = mesh.GetIndices(i); //TODO 이거 nativeArray PTR로 복사비용 없앨 수 없나?
+            List<Vector3Int> cells = indices.Select((elem, index) => new { Elem = elem, Index = index })
+                .GroupBy(x => x.Index / 3)
+                .Select(group => new Vector3Int(
+                    group.ElementAt(0).Elem,
+                    group.ElementAt(1).Elem,
+                    group.ElementAt(2).Elem)).ToList(); //TODO 이렇게 긴 LINQ, 최선인가..?
+            
+            cellPerSubMeshes.Add(new CellPerSubmesh(cells));
+        }
+
+        return Encode(mesh.bounds, cellPerSubMeshes, positions, uvs, 32);
     }
 
-    static POPBuffer Encode(Bounds boundingBox, List<Vector3Int> cells, List<Vector3> positions, List<Vector2> uvs, int maxLevel)
+    static POPBuffer Encode(Bounds boundingBox, List<CellPerSubmesh> cellPerSubMeshes, List<Vector3> positions, List<Vector2> uvs, int maxLevel)
     {
-        // Bounds boundingBox = ComputeBoundingBox(positions);
-
-        List<List<Vector3Int>> buckets = BuildBuckets(boundingBox, cells, positions, maxLevel);
+        List<CellPerLevel> buckets = BuildBuckets(boundingBox, cellPerSubMeshes, positions, maxLevel);
         List<Level> levels = BuildLevels(buckets, positions, uvs);
 
         return new POPBuffer() { boundingBox = boundingBox, levels = levels };
@@ -204,31 +249,42 @@ class POPBuffer
         return new Bounds((min + max) / 2, max - min);
     }
 
-    static List<List<Vector3Int>> BuildBuckets(Bounds boundingBox, List<Vector3Int> cells, List<Vector3> positions, int maxLevel)
+    static List<CellPerLevel> BuildBuckets(Bounds boundingBox, List<CellPerSubmesh> cellPerSubMeshes, List<Vector3> positions, int maxLevel)
     {
-        int[] cellLevels = Enumerable.Repeat(-1, cells.Count).ToArray();
-
-        for (int level = maxLevel; level > 0; level--)
+        List<int[]> cellLevelPerSubMeshes = new List<int[]>(cellPerSubMeshes.Count); //cellLevelPerSubMeshes[0] => subMesh0의 각 cell들의 quantizationLevel을 의미. 
+        for (int i = 0; i < cellPerSubMeshes.Count; i++)
         {
-            List<Vector3> quantizedPositions = QuantizeVertices(positions, level, boundingBox);
-            List<int> cellIndices = ListNonDegenerateCells(cells, quantizedPositions);
-
-            foreach (var index in cellIndices)
+            CellPerSubmesh cellPerSubMesh = cellPerSubMeshes[i];
+            int[] cellLevels = Enumerable.Repeat(-1, cellPerSubMesh.cells.Count).ToArray();
+            for (int level = maxLevel; level > 0; level--)
             {
-                cellLevels[index] = level;
+                List<Vector3> quantizedPositions = QuantizeVertices(positions, level, boundingBox);
+                List<int> cellIndices = ListNonDegenerateCells(cellPerSubMesh.cells, quantizedPositions);
+
+                foreach (var index in cellIndices)
+                {
+                    cellLevels[index] = level;
+                }
+            }
+
+            cellLevelPerSubMeshes.Add(cellLevels);
+        }
+        
+        List<CellPerLevel> buckets = Enumerable.Range(0, maxLevel).Select(_ => new CellPerLevel(cellLevelPerSubMeshes.Count)).ToList(); 
+        //buckets[0] => 0레벨에서의 subMesh0과 subMesh1의 삼각형들을 가지고 있음. 
+        for (int i = 0; i < cellLevelPerSubMeshes.Count; i++)
+        {
+            int[] cellLevels = cellLevelPerSubMeshes[i]; //i번째 subMesh의 cell들의 quantizationLevel
+            for(int j = 0; j < cellLevels.Length; j++) 
+            {
+                int cellLevel = cellLevels[j]; //j번째 cell(삼각형)의 quantizationLevel
+                if(cellLevel != -1) 
+                {
+                    buckets[cellLevel - 1].cellPerSubmeshes[i].cells.Add(cellPerSubMeshes[i].cells[j]);
+                }
             }
         }
-
-        List<List<Vector3Int>> buckets = Enumerable.Range(0, maxLevel).Select(_ => new List<Vector3Int>()).ToList();
-        for(int i = 0; i < cellLevels.Length; i++) 
-        {
-            var cellLevel = cellLevels[i];
-            if(cellLevel != -1) 
-            {
-                buckets[cellLevel - 1].Add(cells[i]);
-            }
-        }
-
+        
         return buckets;
     }
 
@@ -292,7 +348,7 @@ class POPBuffer
         return a == b || b == c  || c == a;
     }
     
-    static List<Level> BuildLevels(List<List<Vector3Int>> buckets, List<Vector3> positions, List<Vector2> uvs) //TODO 이게 최선? 
+    static List<Level> BuildLevels(List<CellPerLevel> buckets, List<Vector3> positions, List<Vector2> uvs) //TODO 이게 최선? 
     {
         List<Level> levels = new List<Level>(buckets.Count);
         Dictionary<int, int> indexLookup = new Dictionary<int, int>();
@@ -300,30 +356,38 @@ class POPBuffer
         
         for (int i = 0; i < buckets.Count; i++)
         {
-            List<Vector3Int> cells = buckets[i];
-            List<Vector3Int> newCells = new List<Vector3Int>();
+            CellPerLevel cellPerLevel = buckets[i];
+            CellPerLevel newCellPerLevel = new CellPerLevel(0);
             List<Vector3> newPositions = new List<Vector3>();
             List<Vector2> newUVs = new List<Vector2>();
             
-            for (int j = 0; j < cells.Count; j++)
+            for (int j = 0; j < cellPerLevel.cellPerSubmeshes.Count; j++)
             {
-                Vector3Int tri = cells[j];
-                Vector3Int newTri = Vector3Int.zero;
-                for (int k = 0; k < 3; k++)
+                List<Vector3Int> cells = cellPerLevel.cellPerSubmeshes[j].cells;
+                List<Vector3Int> newCells = new List<Vector3Int>();
+                
+                for (int k = 0; k < cells.Count; k++)
                 {
-                    if (!indexLookup.ContainsKey(tri[k]))
+                    Vector3Int tri = cells[k];
+                    Vector3Int newTri = Vector3Int.zero;
+                    for (int l = 0; l < 3; l++)
                     {
-                        newPositions.Add(positions[tri[k]]);
-                        newUVs.Add(uvs[tri[k]]);
-                        indexLookup.Add(tri[k], lastIndex++);
+                        if (!indexLookup.ContainsKey(tri[l]))
+                        {
+                            newPositions.Add(positions[tri[l]]);
+                            newUVs.Add(uvs[tri[l]]);
+                            indexLookup.Add(tri[l], lastIndex++);
+                        }
+                        newTri[l] = indexLookup[tri[l]];
                     }
-                    newTri[k] = indexLookup[tri[k]];
+                
+                    newCells.Add(newTri);
                 }
                 
-                newCells.Add(newTri);
+                newCellPerLevel.cellPerSubmeshes.Add(new CellPerSubmesh(newCells));
             }
             
-            levels.Add(new Level(newCells, newPositions, newUVs));
+            levels.Add(new Level(newCellPerLevel, newPositions, newUVs));
         }
         return levels;
     }
